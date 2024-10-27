c77d678c295039f0ff590cc2febe604d7724d29f
tegarswasono <tegar.s@weefer.co.id>
2024-10-27 21:27:59 +0700

    cetak userid yg connect ke hub

--- NotificationApp/Hubs/NotificationHub.cs
+++ NotificationApp/Hubs/NotificationHub.cs
@@ -4,6 +4,15 @@ namespace NotificationApp.Hubs
 {
     public class NotificationHub : Hub
     {
+        public override Task OnConnectedAsync()
+        {
+            var userId = Context.UserIdentifier;
 
+            Console.WriteLine(Context.UserIdentifier);
+            Console.WriteLine(Context.ConnectionId);
+            Console.WriteLine(Context.User);
+
+            return base.OnConnectedAsync();
+        }
     }
 }
